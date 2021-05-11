8eaad333cf71ba6a255a596d4a4cbbdfb4e323eb
Aïmane <elkira.aimane@gmail.com>
2021-05-11 10:31:20 +0000

    an XPath verisi

--- sonar-sapcx-items/src/main/java/com/sqli/sapcx/checks/CheckList.java
+++ sonar-sapcx-items/src/main/java/com/sqli/sapcx/checks/CheckList.java
@@ -10,7 +10,7 @@ public class CheckList {
 
     public static List<Class<?>> getCheckClasses() {
         return Arrays.asList(
-                DeploymentTypeCodeCheck.class
+                XPathDeploymentTypeCodeCheck.class
         );
     }
 
