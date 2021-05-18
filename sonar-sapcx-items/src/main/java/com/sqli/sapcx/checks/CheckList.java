4b640a85c92c60371cc5a033c297324b7e1a77f6
allamimouad <allamimouad@gmail.com>
2021-05-18 18:43:37 +0100

    CREATE RULE/TEST : 17-use-collectiontype-set-on-any-side-of-a-relation-that-has-cardinality-many-where-items-must-not-appear-in-that-relation-multiple-times

--- sonar-sapcx-items/src/main/java/com/sqli/sapcx/checks/CheckList.java
+++ sonar-sapcx-items/src/main/java/com/sqli/sapcx/checks/CheckList.java
@@ -10,6 +10,7 @@ public class CheckList {
 
     public static List<Class<?>> getCheckClasses() {
         return Arrays.asList(
+                CollectionTypeSetOnAnyRelationWithCardinalityManyCheck.class,
                 DeploymentTypeCodeCheck.class,
                 CodeShouldNotStartWithGeneratedCheck.class
         );
